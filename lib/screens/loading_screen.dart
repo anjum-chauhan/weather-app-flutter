a355482329cc92437259678d94217e73491c57b0
Anjum Chauhan <anjum.chauhan@nagarro.com>
2020-08-05 16:49:22 +0530

    initial commit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import '../services/location.dart';
+import '../services/networking.dart';
+import '../services/networking.dart';
+
+const kApiKey = '9d6be499b4af280a1a3f0e666ca4d1d4';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,16 +14,38 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kApiKey';
+    NetworkHelper networkData = NetworkHelper(url: url);
+    print(url);
+    var weatherData = networkData.getData();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoadingScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+      body: SafeArea(
+        child: Container(),
       ),
     );
   }
